09ca4e94d6015346a6d166a5e71a7a35ea9ab3e1
CrashCortez <46716213+CrashCortez@users.noreply.github.com>
2019-10-25 09:58:09 -0700

    Add files via upload

---
+++ rpi vlc setup scripts/RunPSVITA.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+vlc v4l2:///dev/video0 :v4l2-standard= :live-caching=0
---
+++ rpi vlc setup scripts/s1.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo apt-get remove vlc libvlc-bin libvlc5 vlc-bin vlc-data vlc-l10n vlc-plugin-base vlc-plugin-notify vlc-plugin-qt vlc-plugin-samba vlc-plugin-skins2 vlc-plugin-video-output vlc-plugin-video-splitter vlc-plugin-visualization
+reboot
